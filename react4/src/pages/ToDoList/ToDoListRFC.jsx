dd3531cd1751859ef1aa132147e632b5d4b56e4d
dnh2703 <dnh2703@gmail.com>
2022-08-19 16:25:08 +0700

    bai13-todolist-gettask-rfc

--- react4/src/pages/ToDoList/ToDoListRFC.jsx
+++ react4/src/pages/ToDoList/ToDoListRFC.jsx
@@ -1,13 +1,191 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 export default function ToDoListRFC() {
+  let [state, setState] = useState([
+    {
+      taskList: [],
+      values: {
+        taskName: "",
+      },
+      errors: {
+        taskName: "",
+      },
+    },
+  ]);
+
+  const getTaskList = () => {
+    let promise = Axios({
+      url: "http://svcy.myclass.vn/api/ToDoList/GetAllTask",
+      method: "GET",
+    });
+    promise.then((result) => {
+      console.log("result", result.data);
+      //   Nếu gọi API lấy về kết quả thành công
+      // => set lại state của component
+      setState({
+        ...state,
+        taskList: result.data,
+      });
+      console.log("thành công");
+    });
+    promise.catch((err) => {
+      console.log("thất bại");
+      console.log("err", err.response.data);
+    });
+  };
+
+  const renderTaskToDo = () => {
+    return state.taskList
+      .filter((item) => !item.status)
+      .map((item, index) => {
+        return (
+          <li key={index}>
+            <span>{item.taskName}</span>
+            <div className="buttons">
+              <button
+                className="remove"
+                type="button"
+                onClick={() => {
+                  delTask(item.taskName);
+                }}
+              >
+                <i className="fa fa-trash-alt" />
+              </button>
+              <button
+                className="complete"
+                type="button"
+                onClick={() => {
+                  doneTask(item.taskName);
+                }}
+              >
+                <i className="far fa-check-circle" />
+              </button>
+            </div>
+          </li>
+        );
+      });
+  };
+
+  const renderTaskDone = () => {
+    return state.taskList
+      .filter((item) => item.status)
+      .map((item, index) => {
+        return (
+          <li key={index}>
+            <span>{item.taskName}</span>
+            <div className="buttons">
+              <button
+                className="remove"
+                type="button"
+                onClick={() => {
+                  delTask(item.taskName);
+                }}
+              >
+                <i className="fa fa-trash-alt" />
+              </button>
+              <button
+                className="complete"
+                type="button"
+                onClick={() => {
+                  rejectTask(item.taskName);
+                }}
+              >
+                <i className="fas fa-check-circle" />
+              </button>
+            </div>
+          </li>
+        );
+      });
+  };
+
+  //   Hàm xử lý xoá task
+  const delTask = (taskName) => {
+    let promise = Axios({
+      url: `http://svcy.myclass.vn/api/ToDoList/deleteTask?taskName=${taskName}`,
+      method: "DELETE",
+    });
+    promise.then((result) => {
+      console.log(result.data);
+      this.getTaskList();
+    });
+    promise.catch((err) => {
+      console.log(err.response.data);
+    });
+  };
+
+  //   Hàm xử lý done task
+  const doneTask = (taskName) => {
+    let promise = Axios({
+      url: `http://svcy.myclass.vn/api/ToDoList/doneTask?taskName=${taskName}`,
+      method: "PUT",
+    });
+    promise.then((result) => {
+      this.getTaskList();
+    });
+    promise.catch((errors) => {
+      console.log(errors.response.data);
+      alert(errors.response.data);
+    });
+  };
+
+  //   Hàm xử lý reject task
+  const rejectTask = (taskName) => {
+    let promise = Axios({
+      url: `http://svcy.myclass.vn/api/ToDoList/rejectTask?taskName=${taskName}`,
+      method: "PUT",
+    });
+    promise.then((result) => {
+      console.log(result.data);
+      this.getTaskList();
+    });
+    promise.catch((errors) => {
+      console.log(errors.response.data);
+      alert(errors.response.data);
+    });
+  };
+
+  console.log(state);
+  const handleChange = (e) => {
+    let { value, name } = e.target;
+    console.log(value, name);
+    let newValues = { ...state.values };
+
+    newValues = { ...newValues, [name]: value };
+
+    let newErrors = { ...state.errors };
+
+    let regexString = /^[a-z A-Z]+$/;
+
+    if (!regexString.test(value) || value.trim() === "") {
+      newErrors[name] = name + "is invalid!";
+    } else {
+      newErrors[name] = "";
+    }
+
+    setState({
+      ...state,
+      values: newValues,
+      errors: newErrors,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    getTaskList();
+    return () => {};
+  }, []);
+
   return (
     <div className="card">
       <div className="card__header">
-        <img src={require("./bg.png")} />
+        <img src={require("./bg.png")} alt=''/>
       </div>
       {/* <h2>hello!</h2> */}
-      <div className="card__body">
+      <form className="card__body">
         <div className="card__content">
           <div className="card__title">
             <h2>My Tasks</h2>
@@ -15,8 +193,10 @@ export default function ToDoListRFC() {
           </div>
           <div className="card__add">
             <input
+              name="taskName"
               id="newTask"
               type="text"
+              onChange={handleChange}
               placeholder="Enter an activity..."
             />
             <button id="addItem">
@@ -26,37 +206,15 @@ export default function ToDoListRFC() {
           <div className="card__todo">
             {/* Uncompleted tasks */}
             <ul className="todo" id="todo">
-              <li>
-                <span>Đi ngủ</span>
-                <div className="buttons">
-                  <button className="remove">
-                    <i className="fa fa-trash-alt" />
-                  </button>
-                  <button className="complete">
-                    <i className="far fa-check-circle" />
-                    <i className="fas fa-check-circle" />
-                  </button>
-                </div>
-              </li>
+              {renderTaskToDo()}
             </ul>
             {/* Completed tasks */}
             <ul className="todo" id="completed">
-              <li>
-                <span>Ăn sáng</span>
-                <div className="buttons">
-                  <button className="remove">
-                    <i className="fa fa-trash-alt" />
-                  </button>
-                  <button className="complete">
-                    <i className="far fa-check-circle" />
-                    <i className="fas fa-check-circle" />
-                  </button>
-                </div>
-              </li>
+              {renderTaskDone()}
             </ul>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
